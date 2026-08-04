9c93ec7c0b43a4f9197a8372328bc857f8e57db0
pipejack2002-alt <pipejack2002@gmail.com>
2026-08-03 22:10:20 -0500

    Add files via upload

---
+++ supabaseClient.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = "https://lehtlxevhzhxohhvjfsf.supabase.co";
+const SUPABASE_KEY = "sb_publishable_okt2oTT0PvJEnwVMwkzRwQ_K710amtX";
+
+const { createClient } = supabase;
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
